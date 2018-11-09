81a2d6f585944a8abf6af55c264406531c7c8d0b
Sandy Edwards <sedwardscode@gmail.com>
2018-11-09 13:10:14 -0500

    added playground file

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,9 @@
+var favoriteWord = "essential"
+var favoriteColor = "purple"
+var favoriteMusician = "Slowbeam"
+
+print(favoriteWord)
+print(favoriteColor)
+print(favoriteMusician)
+
+print("the sky is \(favoriteColor)")
